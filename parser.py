e995832238830add6f6a1ab1667eb01f543945b7
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-01-11 09:04:30 -0600

    Assignment Commit
    added comments and a main to python file

--- parser.py
+++ parser.py
@@ -1,8 +1,24 @@
-file = "CNFExpressions.txt"
-inFile = open(file, 'r')
-print("CNF Expressions", inFile)
-count = 0
-for i in inFile:
-	if(inFile):
-		count+=1
-		print(count, ":", i)
+#python parser script 
+#made by: Brock Barlow
+#date created: 1/11/2017
+
+def main():
+	#read file
+	file = "CNFExpressions.txt"
+	inFile = open(file, 'r')
+	lines = inFile.readlines()
+	inFile.close()
+	
+	#placeholder
+	#
+	
+	#display results
+	print(inFile)
+main()
+
+#print("CNF Expressions", inFile)
+#count = 0
+#for i in inFile:
+#	if(inFile):
+#		count+=1
+#		print(count, ":", i)
