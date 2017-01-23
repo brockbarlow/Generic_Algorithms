611ef8d2576584b4ee6a2de10ea96d312e93a215
Brock Barlow <brock.barlow@students.aie.edu.au>
2017-01-23 12:15:04 -0600

    Assignment Commit
    edited test cnf file.

--- parser.py
+++ parser.py
@@ -10,65 +10,93 @@ import random #import random.
 #Classes.
 class Canidate(object):
 	def __init__(self, string):
+		#Strings.
 		bitString = "";
+		
 		for bit in string:
 			bitString += bit;
 		self.value = bitString;
 		
 	def EvaluateData(self, clauseList, literalList, value):
+		#Strings.
 		tempString = "";
-		chromosomeValue = 0;
+		printString = "";
+		finalString = "";
+		
+		#Bools.
+		inverse = "false";
+		
+		#Data holders.
+		chromosomeValue = None;
+		
 		for string in clauseList:
+			
 			for char in string:
+				
 				if char == '?':
 					tempString += '?';
 				elif char == '&':
 					tempString += '&';
 				elif char == 'V':
 					tempString += 'V';
+				
 				else:
 					index = FindIndex(literalList, char);
 					tempString += str(value.value[index]);
-			inverse = "false";
-			printString = "";
-			finalString = "";
+			
 			for char in tempString:
+				
 				if inverse == "true":
+					
 					if char == '1':
 						finalString += str(0);
 					elif char == '0':
 						finalString += str(1);
 					inverse = "false";
+				
 				elif char == '?' and inverse == "false":
 					inverse = "true";
+				
 				else:
 					finalString += char;
+			
 			chromosomeValue += (eval(finalString));
 			printString += finalString;
 			tempString = "";
 			finalString = "";
+		
 		print(printString);
 		return chromosomeValue / len(literalList);
 		
 	def InverseOffspring(self, literalList, value):
 		for bit in range(len(literalList) / 2, len(literalList)):
+			
 			if value[bit] == '0':
 				value[bit - (len(literalList) / 2)] = '1';
 			elif value[bit] == '1':
 				value[bit - (len(literalList) / 2)] = '0';
+		
 		return value;
 		
 	def MutateValues(self, chancevalue):
+		#Lists.
 		finalList = [];
+		
+		#Strings.
 		bitString = "";
+		
 		for bit in self.value:
+			
 			if random.randrange(1, 101, 1) <= chancevalue:
+				
 				if bit == '1':
 					finalList.append('0');
 				elif bit == '0':
 					finalList.append('1');
+			
 			else:
 				finalList.append(bit);
+		
 		for bit in finalList:
 			bitString += bit;
 		return bitString;
@@ -95,7 +123,7 @@ def GenerateValues(itemLength): #Generate values function. Takes in the length o
 		valueList.append(tempString);
 	return valueList;
 			
-def AddToList(item): #Add to list function. Takes in a item.
+def AddToList(item): #Add to list function. Takes in an item.
 	list = []; #List of items.
 	for char in item.value: #For every character in items value...
 		list.append(char); #add the character to the list.
@@ -106,7 +134,6 @@ def Print(list): #Print function. Takes in a list.
 		print(item); #print the item.
 
 def Main(): #Main function.
-
 	#Files.
 	fileOne = "CNFExpressions.txt"; #Name of file one. Holds cnf expressions.
 	fileTwo = "TestCNFExpression.txt"; #Name of file two. Holds a test cnf expression.
@@ -142,6 +169,7 @@ def Main(): #Main function.
 			
 			#Data holders.
 			solution = None; #Solution variable. At start, equals nothing. Will be used to hold the canidate object.
+			populationCounter = None; #Counter variable. At start, equals nothing. Holds breedable list length.
 			
 			for char in string: #For each character in the string, do the following.
 				
@@ -175,7 +203,9 @@ def Main(): #Main function.
 			populationList = GenerateValues(len(literalList));
 			
 			while(foundSolution == "false"):
+				
 				for canidate in populationList:
+					
 					if canidate.EvaluateData(clauseList, literalList, canidate) >= 1:
 						print(canidate.EvaluateData(clauseList, literalList, canidate));
 						solution = canidate;
@@ -186,12 +216,14 @@ def Main(): #Main function.
 					breedableList.append(canidate);
 				
 				populationCounter = len(breedableList);
+				
 				for parent in range(0, populationCounter / 2):
 					parentOne = breedableList[parent];
 					parentTwo = Canidate("");
 					
 					while active == "true":
 						parentTwo = breedableList[random.randrange(parent, len(breedableList))];
+						
 						if parentTwo in breedableList:
 							active = "false";
 					
@@ -199,24 +231,32 @@ def Main(): #Main function.
 					childTwo = Canidate(parentOne.InverseOffspring(literalList, AddToList(parentOne)));
 					newPopulationList.append(childOne);
 					newPopulationList.append(childTwo);
+					
 					if parentOne in breedableList:
 						breedableList.remove(parentOne);
+					
 					if parentTwo in breedableList:
 						breedableList.remove(parentTwo);
+					
 					for canidate in newPopulationList:
 						
 						for bit in canidate.value:
 							bitString += bit;
 						canidate.value = bitString;
+				
 				for newCanidate in newPopulationList:
 					newCanidate.value = newCanidate.MutateValues(14);
 					populationList.append(newCanidate);
 				
 				for canidate in populationList:
+					
 					if len(finalPopulationList) < 4:
 						finalPopulationList.append(canidate);
+					
 					else:
+						
 						for check in finalPopulationList:
+							
 							if canidate.EvaluateData(clauseList, literalList, canidate) > check.EvaluateData(clauseList, literalList, canidate):
 								check = canidate;
 								
